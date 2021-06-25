44f4433c4d098a22b852fe18b66e7120c58856bc
rajeshsathyakumar <rajsathya.prof@gmail.com>
2021-06-25 15:41:53 -0700

    first commit

---
+++ src/main.js
@@ -0,0 +1,27 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import App from './App.vue'
+import store from './store'
+import AuthHandler from './components/AuthHandler.vue'
+import ImageList from './components/ImageList.vue'
+import UploadForm from './components/UploadForm.vue'
+
+Vue.use(VueRouter)
+
+export const router = new VueRouter({
+    mode: 'history',
+    routes: [
+        { path: '/', component: ImageList },
+        { path: '/upload', component: UploadForm },
+        { path: '/oauth2/callback', component: AuthHandler },
+    ]
+})
+
+new Vue({
+    el: '#app',
+    router,
+    store,
+    render: function (MyApp) {
+        return MyApp(App)
+    }
+})
